8df47defc4df9a7b4c804e9243a1708e76872bd8
Manuel Pfemeter <manne@users.noreply.github.com>
2019-12-31 11:33:17 +0100

    Add example

---
+++ src/SemanticTypes.Consumer/NetCoreConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Obviously.SemanticTypes.Generator;
+
+namespace NetCoreConsole
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            await Console.Out.WriteLineAsync("Hello! Please enter your email address");
+            var enteredEmailAddressOne = await Console.In.ReadLineAsync();
+            var mailOne = new EmailAddress(enteredEmailAddressOne);
+            await Console.Out.WriteLineAsync($"You entered {mailOne}");
+
+            await Console.Out.WriteLineAsync("Hello! Please enter your email address, the address will be checked for being well-formed");
+            var enteredEmailAddressTwo = await Console.In.ReadLineAsync();
+            try
+            {
+                var mailTwo = new EmailAddressWithValidation(enteredEmailAddressTwo);
+                await Console.Out.WriteLineAsync($"You entered a well-formed {mailTwo}");
+            }
+            catch (ArgumentException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                await Console.Out.WriteLineAsync($"You entered a not well-formed email address. Parameter: {e.ParamName}. Exception message: {e.Message}");
+                Console.ResetColor();
+            }
+        }
+    }
+
+    [SemanticType(typeof(string))]
+    public partial class EmailAddress { }
+
+    [SemanticType(typeof(string))]
+    public partial class EmailAddressWithValidation
+    {
+        public static bool IsValid(string value)
+        {
+            return value.Contains('@', StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
